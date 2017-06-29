17deafbd25726f4c527872a2701efd81ab6c63f8
Wes Van Duine <Wes Van Duine>
2017-06-28 21:18:38 -0700

    added npm variables

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+/////////////////
+//NPM Variables//
+/////////////////
+var inquirer = ('inquirer');
+var fs = ('fs');
+var basic = require("./BasicCard");
+var cloze = require("./ClozeCard");
